204124a4585422109f793c6d788b65676004d39e
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-10-07 12:07:56 +0300

    ADD api get requests model and services

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -1,12 +1,14 @@
-import 'dart:convert';
 import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class AllProductsService {
-  Future<List<ProductModel>> getAllProducts() async {
-    List<dynamic> data =
-        await Api().get(url: 'https://fakestoreapi.com/products');
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProducts(
+      {required String categoryName}) async {
+    List<dynamic> data = await Api()
+        .get(url: 'https://fakestoreapi.com/products/category/$categoryName');
+
     List<ProductModel> productList = [];
     for (int i = 0; i < data.length; i++) {
       productList.add(
