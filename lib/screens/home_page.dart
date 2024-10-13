6e44c6464bc33f092f528adec1102f4fbac6f81e
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-10-13 09:58:49 +0300

    ADD home page ui

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:store_app/widgets/custom_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static String id = 'HomePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.black45,
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(
+            color: Colors.black45,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 85),
+        child: GridView.builder(
+            clipBehavior: Clip.none,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 100,
+            ),
+            itemBuilder: (context, index) {
+              return CustomCard();
+            }),
+      ),
+    );
+  }
+}
